995676f3b6305c3221b41d4a1f9b301bce15a4cc
slepec <slepec2018@gmail.com>
2023-12-10 17:14:53 +0000

    add_input_in_header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/outline";
 
 export default function Header() {
   return (
@@ -22,6 +23,25 @@ export default function Header() {
           layout="fill"
           className="object-contain"
         />
+      </div>
+
+      <div
+        className="relative mt-1"
+      >
+        <div
+          className="absolute top-2 left-2"
+        >
+          <SearchIcon
+            className="h-5 text-gray-500"
+          />
+        </div>
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+        >
+
+        </input>
       </div>
 
       <h1>Right side</h1>
