3ba83511f7710b8aa0fa27d20999ab03bfaee926
slepec <slepec2018@gmail.com>
2023-12-17 13:03:46 +0000

    start_work_with_post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,32 @@
-export default function Post({img, userImg, caption, userName, id}) {
+import { DotsHorizontalIcon } from "@heroicons/react/outline"
+
+export default function Post({ img, userImg, caption, userName, id }) {
   return (
-    <div>
-      <h1>{userName}</h1>
+    <div
+      className="bg-white my-7 border rounded-md "
+    >
+      <div
+        className="flex items-center p-5"
+      >
+        <img
+          src={userImg}
+          alt={userName}
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+        />
+        <p
+          className="font-bold flex-1"
+        >
+          {userName}
+        </p>
+        <DotsHorizontalIcon
+          className="h-5"
+        />
+      </div>
+
+      <img
+        src={img}
+        className="object-cover w-full"
+      />
     </div>
   )
 }
